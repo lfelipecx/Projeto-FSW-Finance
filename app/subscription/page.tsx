ca4c4ba0343efa0a110890858935de8be9fbbcf9
Luiz Felipe <lfelipecx@gmail.com>
2024-11-10 17:48:25 -0300

    feat: ensure transactions and subscriptions pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,11 +1,17 @@
-import Navbar from "../_components/navbar"
+import { auth } from "@clerk/nextjs/server";
+import Navbar from "../_components/navbar";
+import { redirect } from "next/navigation";
 
-const SubscriptionPage = () => {
-    return (
-        <>
-            <Navbar />
-        </>
-    )
-}
+const SubscriptionPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
 
-export default SubscriptionPage
+export default SubscriptionPage;
